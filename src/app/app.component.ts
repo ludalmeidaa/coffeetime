0a06f4a630233b40ac040b115ea7ab86729917ed
ludalmeidaa <ludalmeidaa03@gmail.com>
2025-04-27 13:09:00 -0300

    feat: adiciona footer e edita card-carrosel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { CardCarouselComponent } from './card-carousel/card-carousel.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent],
+  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
