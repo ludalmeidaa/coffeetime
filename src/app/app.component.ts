1bdcd4e99ae428147ed1fdb881f3b6c5177e7ca7
ludalmeidaa <ludalmeidaa03@gmail.com>
2025-05-06 22:00:39 -0300

    feat: adiciona o cardapio e ferramenta tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { CardCarouselComponent } from './card-carousel/card-carousel.component';
 import { FooterComponent } from "./footer/footer.component";
+import { CardapioComponent } from "./cardapio/cardapio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent, FooterComponent, CardapioComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        aleppo: ['Chau Philomene One', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
