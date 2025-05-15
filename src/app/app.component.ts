1940e4f12ffba2069cdf017646e7e0791c1cabb0
Joaoagostinhof <joao.agostinho@cortex-intelligence.com>
2025-05-14 22:35:36 -0300

    Alterações no carrossel e nova seção "Sobre nós"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { CardCarouselComponent } from './card-carousel/card-carousel.component';
 import { FooterComponent } from "./footer/footer.component";
 import { CardapioComponent } from "./cardapio/cardapio.component";
+import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent, FooterComponent, CardapioComponent],
+  imports: [RouterOutlet, HeaderComponent, CardCarouselComponent, FooterComponent, CardapioComponent, SobreNosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+
       fontFamily: {
         aleppo: ['Chau Philomene One', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
+        barlow: ['Barlow', 'sans-serif'], 
+      },
+
+      colors: {
+        'marrom-principal': '#361005',
+        'amarelo-destaque': '#F7CB60',
+        'fundo-carrossel': '#fffaf3', 
       },
     },
   },
